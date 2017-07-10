23b15d84098960e90a633410f5f218ff6d73ef2f
A <a@as-macbook-air>
2017-07-10 13:40:21 +0300

    Initial Commit

---
+++ SurveyApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SurveyApp
+//
+//  Created by A on 10/07/2017.
+//  Copyright © 2017 EmailsGmails. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ SurveyApp/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  SurveyApp
+//
+//  Created by A on 10/07/2017.
+//  Copyright © 2017 EmailsGmails. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
